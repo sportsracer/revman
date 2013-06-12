b1817de54a499b2dcab5f2b3cbb9ef3e4cf1b9dd
Steffen Wenz <swenz@trustyou.net>
2013-06-12 21:16:47 +0200

    Removal of Trust Score

--- model/hotel.go
+++ model/hotel.go
@@ -8,7 +8,7 @@ const (
 	// cost of renting out a room
 	VariableCost = 80
 	// fix cost of operating for a day
-	FixCost = 1000
+	FixCost = 800
 	// daily interest rate on credits
 	InterestRate = 0.01
 )
@@ -16,21 +16,15 @@ const (
 type Hotel struct {
 	Rooms int
 	Balance float32
-	TrustScore int
-}
-
-func (h *Hotel) getReputationValue() float32 {
-	return (float32(h.TrustScore) - 70.0) * 0.25
 }
 
 func MakeHotel() *Hotel {
 	return &Hotel{
 		Rooms: 100,
 		Balance: 0,
-		TrustScore: 80,
 	}
 }
 
 func (h *Hotel) String() string {
-	return fmt.Sprintf("Hotel (rooms: %d, balance: %f, TrustScore: %d)", h.Rooms, h.Balance, h.TrustScore)
+	return fmt.Sprintf("Hotel (rooms: %d, balance: %f)", h.Rooms, h.Balance)
 }
