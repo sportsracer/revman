43e624bc56b8996a72a4d82f7c5e52b00aeaaa6d
Steffen Wenz <steffenwenz@gmail.com>
2021-09-30 19:41:47 +0200

    Reformat JavaScript files; load jQuery from CDN

--- static/client.js
+++ static/client.js
@@ -1,115 +1,119 @@
+/**
+ * Visualize the state we receive from the server.
+ */
+const visualize = (function() {
+  // Keep a history from previous rounds so we can plot a graph.
+  const history = {
+    balance: [],
+    boughtOffers: [],
+  };
+  let round = 0;
+  return function(state) {
+    $('#balance').text(state.balance);
+    $('#boughtOffers').text(state.offers.length);
 
-const rooms = 100
-, platforms = ["ta", "book"];
-
-visualize = (function() {
-	var history = {
-		balance: [],
-		boughtOffers: []
-	}
-	, round = 0;
-	return function (state) {
-		$("#balance").text(state.balance);
-		$("#boughtOffers").text(state.offers.length);
-		
-		history.balance.push([round, state.balance]);
-		history.boughtOffers.push([round, state.offers.length]);
-		$.plot("#graph", [{
-			data: history.balance,
-			label: "Balance"
-		}, {
-			data: history.boughtOffers,
-			label: "Bought offers",
-			yaxis: 2
-		}], {
-			yaxes: [ {}, {
-				min: 0, max: 100,
-				position: "right"
-			}]
-		});
-		round++;
-	}
+    history.balance.push([round, state.balance]);
+    history.boughtOffers.push([round, state.offers.length]);
+    $.plot('#graph', [{
+      data: history.balance,
+      label: 'Balance',
+    }, {
+      data: history.boughtOffers,
+      label: 'Bought offers',
+      yaxis: 2,
+    }], {
+      yaxes: [{}, {
+        min: 0, max: 100,
+        position: 'right',
+      }],
+    });
+    round++;
+  };
 })();
 
+/**
+ * Calculate the offers to publish in the next round. Reimplement this with your automatic revenue manager.
+ * @param {Object} state Game state as received from the server
+ * @return {Object[]} List of offers to publish
+ */
 function makeOffers(state) {
-	// make 100 random offers
-	var offers = []
-	, price = parseFloat(document.getElementById("price").value);
-	for (var i = 0; i < 100; i++) {
-		offers.push({
-			platform: platforms[Math.floor(Math.random() * platforms.length)],
-			price: price + Math.random() * 5.0
-		});
-	}
-	return offers;
+  // make 100 random offers
+  const platforms = ['ta', 'book', 'hrs', 'hc'];
+  const offers = [];
+  const price = parseFloat(document.getElementById('price').value);
+  for (let i = 0; i < 100; i++) {
+    offers.push({
+      platform: platforms[Math.floor(Math.random() * platforms.length)],
+      price: price + Math.random() * 20.0,
+    });
+  }
+  return offers;
 }
 
+/**
+ * Display a status message to the player.
+ * @param {String} msg
+ */
 function status(msg) {
-	$("#status").text(msg);
-	console.log(msg);
+  $('#status').text(msg);
+  console.log(msg);
 }
 
+/**
+ * Join a networked game, and set up all required callbacks and
+ * @param {Object} settings Name and websocket URL
+ */
 function joinGame(settings) {
+  status('Connecting to ' + settings.url + ' ...');
 
-	status("Connecting to " + settings.url + " ...");
-	
-	ws = new WebSocket(settings.url);
-	ws.onopen = function() {
-		status("Connection established! Joining as " + settings.name + " ...");
-		ws.send(JSON.stringify(
-				{
-					Msg: "join",
-					Data: {
-						name: settings.name
-					}
-				}
-			));
-	};
-	
-	ws.onmessage = function(message) {
-		message = JSON.parse(message.data);
-		switch (message.Msg) {
-		
-		case "joined":
-			status("Joined as player " + message.Data.playerIndex);
-			$("#setup").hide();
-			$("#game").show();
-			break;
-			
-		case "state":
-			console.log("Received", message);
-			visualize(message.Data);
-			
-			var offers = makeOffers(message.Data)
-			, input = {
-				"Msg": "input",
-				"Data": {
-					"offers": offers
-				}
-			};
-			console.log("Sending", input);
-			ws.send(JSON.stringify(input));
-			break;
-			
-		default:
-			log("Invalid msg");
-		}
-	};
+  ws = new WebSocket(settings.url);
+  ws.onopen = function() {
+    status('Connection established! Joining as ' + settings.name + ' ...');
+    ws.send(JSON.stringify(
+        {
+          Msg: 'join',
+          Data: {
+            name: settings.name,
+          },
+        },
+    ));
+  };
 
+  ws.onmessage = function(message) {
+    message = JSON.parse(message.data);
+    switch (message.Msg) {
+      case 'joined':
+        status('Joined as player ' + message.Data.playerIndex);
+        $('#setup').hide();
+        $('#game').show();
+        break;
+
+      case 'state':
+        console.log('Received', message);
+        visualize(message.Data);
+
+        const offers = makeOffers(message.Data);
+        const input = {
+          'Msg': 'input',
+          'Data': {
+            'offers': offers,
+          },
+        };
+        console.log('Sending', input);
+        ws.send(JSON.stringify(input));
+        break;
+
+      default:
+        log('Invalid msg');
+    }
+  };
 }
 
 $(document).ready(function() {
-	var statusEl = $("#status");
-	
-	function getSettings() {
-		return {
-			name: $("#name").val(),
-			url: $("#url").val()
-		}
-	}
-	
-	$("#join").click(function() {
-		joinGame(getSettings());
-	});
-	
+  $('#join').click(function() {
+    joinGame({
+      name: $('#name').val(),
+      url: $('#url').val(),
+    });
+  });
 });
--- static/spectate.js
+++ static/spectate.js
@@ -1,67 +1,83 @@
+/**
+ * Get and visualize the newest game state.
+ */
 function refresh() {
-	$.ajax({
-		url: "http://localhost:8090/state"	
-	}).done(function(dataStr) {
-		visualize(JSON.parse(dataStr));
-	});
+  $.ajax({
+    url: 'http://localhost:8090/state',
+  }).done(function(dataStr) {
+    visualize(JSON.parse(dataStr));
+  });
 }
 
-const minSize = 10
-, maxSize = 70
-, platforms = ["ta", "book", "hrs", "hc"];
+const minSize = 10;
+const maxSize = 70;
+const platforms = ['ta', 'book', 'hrs', 'hc'];
 
+/**
+ * Format a USD amount for display.
+ * @param {Number} currency
+ * @returns {String}
+ */
+function formatCurrency(currency) {
+	const rounded = Math.round(currency * 100) / 100;
+	return `$${rounded}`;
+}
+
+/**
+ * Visualize the game state.
+ * @param {Object} data Game state as received from server.
+ */
 function visualize(data) {
-	
-	var minMax = data.players.reduce(function(minMax, player) {
-		if (minMax.min == null || player.balance < minMax.min) {
-			minMax.min = player.balance;
-		}
-		if (minMax.max == null || player.balance > minMax.max) {
-			minMax.max = player.balance;
-		}
-		return minMax;
-	}, {"min": null, "max": null});
-	
-	function calcSize(balance) {
-		return minSize + (maxSize - minSize) * (balance - minMax.min) / (minMax.max - minMax.min);
-	}
-	
-	var playersEl = $("<div>");
-	data.players.forEach(function(player) {
-		playersEl.append(
-			$('<div class="player" style="width: {width}%"><span class="name">{name}</span><br/><span class="balance">{balance}</span></div>'
-				.replace("{name}", player.name)
-				.replace("{balance}", player.balance)
-				.replace("{width}", Math.round(calcSize(player.balance)))
-			)
-		);
-	});
-	$("#players").html(playersEl.html());
-	
-	var offers = {
-		"ta": [], "book": [], "hrs": [], "hc": []
-	};
-	data.offers.sort(function(o1, o2) {
-		return o1.price - o2.price;
-	}).forEach(function(offer) {
-		offers[offer.platform].push(offer);
-	});
-	
-	platforms.forEach(function(platform) {
-		var platformEl = $("<ul>");
-		platformEl.append("<li>TOTAL: {total}</li>".replace("{total}", offers[platform].length));
-		offers[platform].forEach(function(offer) {
-			platformEl.append(
-				$("<li>{price} ({player})</li>"
-					.replace("{price}", offer.price)
-					.replace("{player}", offer.player)
-				)
-			);
-		});
-		$("#" + platform).html(platformEl.html());
-	});
+  const minMax = data.players.reduce(function(minMax, player) {
+    if (minMax.min == null || player.balance < minMax.min) {
+      minMax.min = player.balance;
+    }
+    if (minMax.max == null || player.balance > minMax.max) {
+      minMax.max = player.balance;
+    }
+    return minMax;
+  }, {'min': null, 'max': null});
+
+  function calcSize(balance) {
+    return minSize + (maxSize - minSize) * (balance - minMax.min) / (minMax.max - minMax.min);
+  }
+
+  const playersEl = $('<div>');
+  data.players.forEach(function(player) {
+    playersEl.append(
+        $('<div class="player" style="width: {width}%"><span class="name">{name}</span><br/><span class="balance">{balance}</span></div>'
+            .replace('{name}', player.name)
+            .replace('{balance}', formatCurrency(player.balance))
+            .replace('{width}', Math.round(calcSize(player.balance))),
+        ),
+    );
+  });
+  $('#players').html(playersEl.html());
+
+  const offers = {
+    'ta': [], 'book': [], 'hrs': [], 'hc': [],
+  };
+  data.offers.sort(function(o1, o2) {
+    return o1.price - o2.price;
+  }).forEach(function(offer) {
+    offers[offer.platform].push(offer);
+  });
+
+  platforms.forEach(function(platform) {
+    const platformEl = $('<ul>');
+    platformEl.append('<li>TOTAL: {total}</li>'.replace('{total}', offers[platform].length));
+    offers[platform].forEach(function(offer) {
+      platformEl.append(
+          $('<li>{price} ({player})</li>'
+              .replace('{price}', formatCurrency(offer.price))
+              .replace('{player}', offer.player),
+          ),
+      );
+    });
+    $('#' + platform).html(platformEl.html());
+  });
 }
 
 $(document).ready(function() {
-	setInterval(refresh, 1000);
+  setInterval(refresh, 1000);
 });
